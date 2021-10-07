71bc7e61038ddc586e40565eeb2197f6f7b844be
rani <ranikumari1901r@gmail.com>
2021-10-07 11:00:21 -0700

    commit changes razorpay

---
+++ src/planning.js
@@ -0,0 +1,19 @@
+//bookmyshoplanning
+//needa reusable carousel
+//herocaousel
+//commoncaousel
+//castandcrew caousel
+
+//pages--
+//homepage
+//movie
+//categoy
+//authpage
+//paypage
+
+//plan
+//layouts
+//routes
+//pages
+//componets
+//LAYOUTFISTAPPOACH
